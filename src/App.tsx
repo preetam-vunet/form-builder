518009adf1a636a1aff1a7890acbe2075aa502d7
preetam-vunet <preetamv@vunetsystems.com>
2025-08-22 12:22:05 +0530

    Made changes in App.js to import FormBuilder Compoenent

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import './App.css'
+import FormBuilder from "./components/FormBuilder";
+import type { Field } from "./types";
 
-function App() {
+const userFormSchema = [
+  { type: "text", name: "username", label: "Username", required: true },
+  { type: "number", name: "age", label: "Age", min: 18 },
+  { type: "select", name: "role", label: "Role", options: ["Admin", "User"] },
+  { type: "checkbox", name: "isActive", label: "Active" },
+] as const;
 
+export default function App() {
   return (
-    <>
-    </>
-  )
+    <div>
+      <h1>User Form</h1>
+      <FormBuilder
+        schema={userFormSchema as readonly Field[]}
+        onSubmit={(data) => {
+          console.log("Form Data:", data);
+        }}
+      />
+    </div>
+  );
 }
-
-export default App
